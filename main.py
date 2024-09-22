9213d169d82f4cc6957fac72712618935ae68834
cutroy <savisvit@gmail.com>
2024-09-22 20:15:44 +0300

    main

---
+++ main.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import csv
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import RandomForestRegressor
+import random
+
+seed = 1
+
+data = pd.read_csv('train.csv')
+data.fillna(0,inplace=True) 
+X=data[['Широта', 'Долгота', 'Индекс', 'Площадь',
+       'Этаж', 'Размер_участка',
+       'Кво_комнат', 'Кво_спален', 'Кво_ванных', 'Кво_фото', 'Нлч_парковки',
+        'Нлч_балкона', 'Нлч_террасы', 'Нлч_подвала',
+       'Нлч_гаража', 'Нлч_кондиционера']]
+Y = data['Цена']
+x_train,x_test,y_train,y_test = train_test_split(X,Y,test_size=0.01,random_state=1)
+knn = RandomForestRegressor()
+knn.fit(x_train, y_train)
+to_pred = pd.read_csv('public_test.csv')
+to_pred.fillna(0, inplace=True)
+x_to_pred_all = to_pred[['id','Широта', 'Долгота', 'Индекс', 'Площадь',
+       'Этаж', 'Размер_участка', 'Расход_тепла',
+       'Кво_комнат', 'Кво_спален', 'Кво_ванных', 'Кво_фото', 'Нлч_парковки',
+       'Нлч_почтового_ящика', 'Нлч_балкона', 'Нлч_террасы', 'Нлч_подвала',
+       'Нлч_гаража', 'Нлч_кондиционера']]
+x_to_pred = to_pred[['Широта', 'Долгота', 'Индекс', 'Площадь',
+       'Этаж', 'Размер_участка',
+       'Кво_комнат', 'Кво_спален', 'Кво_ванных', 'Кво_фото', 'Нлч_парковки',
+        'Нлч_балкона', 'Нлч_террасы', 'Нлч_подвала',
+       'Нлч_гаража', 'Нлч_кондиционера']]
+y_pred = knn.predict(x_to_pred)
+ans = [[int(x_to_pred_all['id'][i]),int(y_pred[i])] for i in range(len(y_pred))]
+with open('public_sample_submission_seed_1.csv','w',newline='') as f:
+    csv.writer(f).writerow(['id', 'Цена'])
+    csv.writer(f).writerows(ans)
